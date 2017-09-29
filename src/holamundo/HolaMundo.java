65d58f333bb05d17f771d43ed4df9195d29219cb
ccosmesanmartin <ccosmesanmartin@victoria23.danielcastelao.org>
2017-09-29 09:24:12 +0200

    Hola Mundo

--- src/holamundo/HolaMundo.java
+++ src/holamundo/HolaMundo.java
@@ -17,6 +17,7 @@ public class HolaMundo {
     public static void main(String[] args) {
        System.out.println("Hola Mundo ");
        
+       
         // TODO code application logic here
     }
     
